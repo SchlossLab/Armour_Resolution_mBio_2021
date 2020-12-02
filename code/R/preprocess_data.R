d0e3e96d5362dfaf609059f2ea9365b9b2350c7f
courtneyarmour <armourc@med.umich.edu>
2020-12-02 09:12:58 -0500

    add asv filtering

--- code/R/preprocess_data.R
+++ code/R/preprocess_data.R
@@ -23,6 +23,14 @@ print(R.version$version.string)
 outdir <- paste0("./data/",args$taxonomy,"/")
 
 data <- as_tibble(fread(args$data))
+
+# for ASV level data, filter out columns with count 1 
+# these will be removed anyway in preprocessing
+# so this speeds up the processing for large data
+if( args$taxonomy == "asv" ){
+	data <- data[,colSums(data != 0) > 1]
+}
+
 data_proc <- mikropml::preprocess_data(data, 'dx')$dat_transformed
 
 write_csv(data_proc,paste0("./data/",args$taxonomy,"/input_data_preproc.csv"))
