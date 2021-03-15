f3f854faae3c833b66c89782ec60f576a3830535
courtneyarmour <armourc@med.umich.edu>
2021-03-15 10:43:16 -0400

    summarize dada2 data

---
+++ code/R/quantify_input_values_dada2.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(data.table)
+
+levels <- c("dada2")
+
+input <- NULL
+
+for(l in levels){
+  in_data <- as.data.frame(fread(paste0("./data/",l,"/input_data.csv")))
+  in_data_preproc <- as.data.frame(fread(paste0("./data/",l,"/input_data_preproc.csv")))
+
+  n_samp <- nrow(in_data)
+  n_feat <- ncol(in_data) - 1
+  n_feat_preproc <- ncol(in_data_preproc) - 1
+
+  input <- input %>%
+    bind_rows(c(level=l,n_samples=n_samp,n_features=n_feat,n_features_preproc=n_feat_preproc))
+}
+
+write_csv(input,"./data/dada2/process/summary_input_values.csv")
